1f688f6cd03e5ae562c6cb5dde8e8b0d6aeb0864
Kirill <k.varshamov@psw.ru>
2020-12-24 11:17:04 +0300

    Update minimum iOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "EasyPeasy",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v9),
         .macOS(.v10_10),
         .tvOS(.v9)
     ],
